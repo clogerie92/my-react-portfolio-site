c2b792a44a7a07e875603250ebad560a446822e6
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-07 15:37:20 -0400

    created about section in About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import myportrait from "./images/myportrait.png";
+
+function About () {
+    return (
+        <div className="about-section">
+            <h2 id="about-me">About</h2>
+            <img src={myportrait} alt="portrait" id="my-portrait"/>
+            <div className="about-container">
+                <p id="about-content">
+                    Hi! My name is Carl Logerie and I am a passionate full stack
+                    software engineer using web technologies to build amazing web
+                    applications. I love to create and I use software engineering a tool
+                    to solve problems and bring products to life on the internet. I
+                    would love to connect with you! Whether it's a job opportunity,
+                    project, or just to talk. Feel free to connect with me!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
