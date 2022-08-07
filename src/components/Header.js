558190722a590d38577633479b64832976d10b62
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-07 14:36:29 -0400

    added Container, Header, Footer, and Navbar components in components folder. Created a header in Header.js, added styling to header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pages/styles/styles.css";
+
+function Header () {
+    return (
+        <header className="header">
+            <h1 className="my-name">Carl Logerie</h1>
+        </header>
+    )
+}
+
+export default Header;
