bf492d243faab07c85df3622515ce89ddeaa1678
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-07 15:22:35 -0400

    created footer with links to GitHub and LinkedIn profiles in Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./pages/styles/styles.css";
+
+function Footer () {
+    return (
+        <footer className="footer-container">
+            <ul className="footer-list">
+                <li>
+                    <p><a href="https://github.com/clogerie92" rel="noreferrer" target="_blank">GitHub</a></p>
+                </li>
+                <li>
+                    <p><a href="https://www.linkedin.com/in/carl-logerie-a45397245/" rel="noreferrer" target="_blank">LinkedIn</a></p>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
